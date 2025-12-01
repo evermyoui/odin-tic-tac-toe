cc04958a9abad41206eafbdcdc415b0e8ecf131a
said <sexbythewind@gmail.com>
2025-12-01 23:27:05 +0800

    initial functions

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
-const Gameboard = function(){
-    const board = ["","","","","","","","",""];
+function Board(){
+    const row = 3;
+    const column = 3;
+    const board = [];
 
-    function setCell(index, symbol){
-        if (board[index] === ""){
-
+    for(let i = 0; i< row; i++){
+        board[i]= [];
+        for(let j = 0; j < column; j++){
+            board[i].push(Cell());
         }
     }
-} 
+    const printBoard = () => {
+        const boardWithValues = board.map((row)=> row.map((cell)=> cell.getValue()));
+        
+    }
+
+}
+function Cell(){
+    let value = "";
+
+    const addMark = (mark) => {
+        value = mark;
+    }
+    const getValue = () => value;
+
+    return {
+        addMark, getValue
+    }
+}
+function Player(p1 = "PlayerOne", p2 = "PlayerTwo"){
+    const players = [{
+        name: p1,
+        value: "X"
+    }, {
+        name: p2,
+        value: "O"
+    }];
+    
+    let activePlayer = players[0];
+
+    const switchPlayer = () =>{
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+    const getActivePlayer = () => activePlayer;
+
+    return {
+        switchPlayer, getActivePlayer
+    }
+}
+function Game(){
+    const chooseMark = () => {
+        
+    }
+}
